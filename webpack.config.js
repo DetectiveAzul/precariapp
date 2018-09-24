525992f46d29ba1a0efad9ef1be3c99e25e28730
Jaime Lopez <detec.azul@gmail.com>
2018-09-24 12:55:28 +0100

    Now webpack serves react using babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,26 @@
 const config = {
 	entry: `${__dirname}/client/src/index.js`,
+	module: {
+		rules: [
+		  {
+			test: /\.js$/,
+			exclude: /node_modules/,
+			use: {
+			  loader: "babel-loader"
+			}
+		  }
+		]
+	  },
+	resolve: {
+		extensions: ['*', '.js', '.jsx']
+	},
 	output: {
 		path: `${__dirname}/client/public/js`,
 		filename: 'bundle.js'
 	},
+	devServer: {
+		contentBase: `${__dirname}/client/public/`,
+	  },
 	mode: 'development'
 };
 

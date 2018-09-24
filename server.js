79ca438c0922f601d138b57c08bf9c2ff8648656
Jaime Lopez <detec.azul@gmail.com>
2018-09-24 11:06:03 +0100

    Modified eslint to accept console.log statements

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const Koa = require('koa');
 const app = new Koa();
 
-const PORT = process.env.PORT || 3001 ;
+const PORT = process.env.PORT || 3001;
 
 const server = app.listen(PORT, () => {
-    console.log(`Server listening on port: ${PORT}`);
+	console.log(`Server listening on port: ${PORT}`);
 });
+
+module.exports = server;

7812bd755e9375538da0ec9da20310c0928c47b2
Jaime Lopez <detec.azul@gmail.com>
2018-09-24 10:31:53 +0100

    Added koa middleware for serving static files

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const Koa = require('koa');
 const app = new Koa();
+const serve = require('koa-static');
 
-app.use(async ctx => {
-    ctx.body = 'Hello World'
-});
+app.use(serve(__dirname + '/client/index.html'));
 
 app.listen(3000);

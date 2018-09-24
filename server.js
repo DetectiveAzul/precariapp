f26264b76999c2aa00c42e6774e6285f11658b1a
Jaime Lopez <detec.azul@gmail.com>
2018-09-24 11:11:32 +0100

    Adding base router

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 const Koa = require('koa');
 const app = new Koa();
+const router = require('./server/index.js');
+const cors = require('@koa/cors');
+const bodyParser = require('koa-bodyparser');
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
+
+app.use(cors);
+app.use(bodyParser);
+app.use(router.routes());
 
 const server = app.listen(PORT, () => {
 	console.log(`Server listening on port: ${PORT}`);

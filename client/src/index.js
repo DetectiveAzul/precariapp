0573bbe83601bc4aef28df2de3c4873b29509530
Jaime Lopez <detec.azul@gmail.com>
2018-09-25 13:55:14 +0100

    Removed test title on client/src/index.js

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.js';
 
-const title = 'Test Title';
-
-ReactDOM.render(<App>{title}</App>, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));

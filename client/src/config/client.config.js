837e9adfa0f2f66e3f6445835a7441522477c93c
Jaime Lopez <detec.azul@gmail.com>
2018-09-25 14:29:15 +0100

    Adding api endpoint on client/src/config/client.config.js

--- client/src/config/client.config.js
+++ client/src/config/client.config.js
@@ -1,4 +1,4 @@
-// CREATE ON THIS SAME FOLDER A FILE CALLED 'config.js' and copy the following content: 
+// CREATE ON THIS SAME FOLDER A FILE CALLED 'config.js' and copy the following content:
 module.exports = {
-	
+	apiEnd: '/api/v1/'
 };

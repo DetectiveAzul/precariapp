d7ab151eda823df47407ce843aaa55147f058951
Jaime Lopez <detec.azul@gmail.com>
2018-09-25 14:14:45 +0100

    Adding actions to redux/actions/actions.js

---
+++ client/src/_redux/actions/actions.js
@@ -0,0 +1,12 @@
+// import axios from 'axios';
+// import config from '../config/config.js';
+// const ROOT_URL = config.apiEnd;
+
+//CONSTANTS
+export const visibilityFilters = {
+	SHOW_ALL: 'SHOW_ALL',
+	SHOW_OPEN: 'SHOW_OPEN',
+	SHOW_NONE: 'SHOW_NONE'
+};
+
+//ACTIONS
